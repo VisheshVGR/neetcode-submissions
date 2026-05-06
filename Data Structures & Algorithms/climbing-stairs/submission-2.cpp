4a494b56dc2a068b6d33e86c6a4d703c2a60404c
Vishesh Gupta <42836833+VisheshVGR@users.noreply.github.com>
2026-05-06 22:10:04 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    vector<int> dp;
+
+public:
+    int climbStairs(int n)
+    {
+        dp.assign(n + 1, -1);
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == 1)
+                dp[i] = dp[i - 1];
+            else
+                dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
