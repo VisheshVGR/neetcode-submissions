16ec972cac7972e41bcec6a83bbd899ae64f28ad
Vishesh Gupta <42836833+VisheshVGR@users.noreply.github.com>
2026-05-09 14:18:16 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval)
+    {
+        vector<vector<int>> ans;
+
+        vector<vector<int>> interval(2, vector<int>(1005, 0));
+
+        for (auto &data : intervals)
+        {
+            interval[0][data[0]]++;
+            interval[1][data[1]]--;
+        }
+        
+        // Adding new interval
+        interval[0][newInterval[0]]++;
+        interval[1][newInterval[1]]--;
+
+        int currsum = 0;
+
+        int startpoint = -1;
+        int endpoint = -1;
+
+        for (int i = 0; i < 1005; i++)
+        {
+            currsum += interval[0][i];
+
+            if (currsum != 0 && startpoint == -1)
+                startpoint = i;
+
+            currsum += interval[1][i];
+
+            if (currsum == 0 && startpoint != -1)
+            {
+                endpoint = i;
+
+                ans.push_back({startpoint, endpoint});
+
+                startpoint = -1;
+                endpoint = -1;
+            }
+        }
+
+        return ans;
+    }
+};
